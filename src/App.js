bc49f3a0545564c604d95d8bb27baff8991d9bc2
Ramya Challa <ramyac6@cs.washington.edu>
2020-10-17 15:48:20 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import KommunicateChat from './chat'
 
@@ -10,15 +10,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click on the chat bot in the corner to interact with Atlas!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ramyac6/BlackRockreact"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          See the source code
         </a>
       </header>
     </div>
